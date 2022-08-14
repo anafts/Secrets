61554aafb524087ba7c1f1d294b69c30a8120ee1
Ana Freitas <ana.freitaas16@gmail.com>
2022-08-14 11:28:30 -0300

    using passport.js to add Cookies and Sessions

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const bodyParser = require("body-parser");
 const mongoose = require ("mongoose");
 const ejs = require("ejs");
 const dotenv = require("dotenv");
-const bcrypt = require('bcryptjs');
 const session = require('express-session')
 const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const passportLocalMongoose = require('passport-local-mongoose');
-
-const salt = bcrypt.genSaltSync(10);
 
 dotenv.config()
 
@@ -18,6 +16,20 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// set up express session
+app.use(session({
+  secret: "little secret",
+  resave: false,
+  saveUninitialized: false
+}));
+
+// initialize and start using passport.js
+app.use(passport.initialize());
+/// allow passport to use express-session
+app.use(passport.session());
+
+
+//Connect to mongoDB
 mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema ({
@@ -30,7 +42,15 @@ const userSchema = new mongoose.Schema ({
 });
 
 
+// to hash and salt the passwords and to save the users into the mongoDB database
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User" , userSchema);
+
+// passport-local Configuration
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/" , function(req, res){
     res.render("home")
@@ -46,45 +66,67 @@ app.get("/register" , function(req, res) {
     res.render("register")
 });
 
+app.get("/secrets", function(req, res) {
+
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", function(req, res) {
+ 
+  req.logout(function(err) {
+      if (!err) {
+          res.redirect("/");
+      }
+  });
+});
+
 
 app.post("/register"  , function(req, res) {
 
-  const hash = bcrypt.hashSync(req.body.password, salt);
-
-    const newUser = new User ({
-        email: req.body.username ,
-        password: hash
-    })
-    newUser.save(function(err){
-        if (!err){
-        res.render("secrets");
-        } else {
-            console.log(err);
-        }
-    })
+  User.register({username:req.body.username}, req.body.password , function(err, user){
+    if (err) {
+      console.log(err);
+      res.redirect("/register")
+    } else {
+      passport.authenticate("local")(req,res, function(){
+        res.redirect("/secrets")
+      });
+    }
+  });
 });
 
 app.post("/login" , function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
+    
+  User.findOne({username: req.body.username}, function(err, foundUser){
+    if(foundUser){
+      const user = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
 
-    User.findOne({email: username}, (err, foundUser) => {
-        if (err) {
+      passport.authenticate("local", function(err, user){
+        if(err){
           console.log(err);
         } else {
-          if (foundUser) {
-            if (bcrypt.compareSync(req.body.password, foundUser.password)) {
-              res.render("secrets");
-              console.log("New login (" + username + ")");
-            } else {
-              res.render("login", {errMsg: "Email or password incorrect", username: username, password: password});
-            }
+          if(user){
+            req.login(user, function(err){
+            res.redirect("/secrets");
+            });
           } else {
-            res.render("login", {errMsg: "Email or password incorrect", username: username, password: password});
+            res.redirect("/login");
           }
         }
-      });
+      })(req, res);
+    } else {
+      res.redirect("/login")
+    }
+  });
 });
+
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
